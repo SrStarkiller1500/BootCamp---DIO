13c27dc6f94d4d7f15870f6627499467f29734d8
SrStarkiller1500 <rafael.jose.lins@gmail.com>
2024-11-03 18:24:28 -0300

    Atualizando os arquivos novamente

--- Projetos/Collection JAVA/src/EstoqueProdutos.java
+++ Projetos/Collection JAVA/src/EstoqueProdutos.java
@@ -11,7 +11,7 @@ public class EstoqueProdutos {
 
 
     public void adiconarProduto(long cod, String nome, int quantidade, double preco){
-        estoqueProdutosMap.put(cod, new Produto(nome, preco , quantidade));
+        estoqueProdutosMap.put(cod, new Produto(quantidade, nome, preco , quantidade));
     }
 
     public void exibirProdutos(){
@@ -30,7 +30,7 @@ public class EstoqueProdutos {
 
     public Produto obterProdutoMaisCaro(){
         Produto produtoMaisCaro = null;
-        double maiorPreco = Double.MIN_VALUE;
+        double maiorPreco = Double.MIN_VALUE;//retorna um valor único
 
         if (!estoqueProdutosMap.isEmpty()){
             for (Produto p: estoqueProdutosMap.values()){
@@ -40,5 +40,21 @@ public class EstoqueProdutos {
             }
         }
         return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+
+
+        estoque.adiconarProduto(1L, "Produto A", 10, 5.0);
+        estoque.adiconarProduto(2L, "Produto B", 5, 10.0);
+        estoque.adiconarProduto(3L, "Produto C", 2, 15.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$ " + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais barato: " + estoque.obterProdutoMaisCaro());
+        
     }
 }
